73d6b777136098c0edd8add1f2a671ba4db0c659
nAguayoValdebenito <n.aguayovaldebenito@gmail.com>
2024-09-24 13:22:13 -0300

    actualizacion2

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../interfaces/usuario';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  urs:Usuario={
+    id: Date.now(),
+    nombre: "",
+    email: "",
+    contrasena: ""
+  }
+  constructor(
+    private AlertCtr : AlertController,
+    private router : Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  login(){
+    if (!this.urs.email || !this.urs.contrasena) {
+      this.alertas("Error", "Rellene los campos");
+      return; 
+    }
+    if(this.urs.email == "javier@conductor.cl" &&  this.urs.contrasena == "123456"){
+       this.alertas("Bienvenido Javier","Conductor")
+       this.router.navigateByUrl("/conductor")
+    }else if(this.urs.email == "javier@alumno.cl" &&  this.urs.contrasena == "123"){
+      this.alertas("Bienvenido Javier","Alumno")
+      this.router.navigateByUrl("/pasajero")
+    }else{
+      this.alertas("Error ","Datos de usuario son incorrectos")
+    }
+  }
+
+  async alertas(mensaje: string,subtitulo:string){
+    const alrt = await this.AlertCtr.create({
+      header: mensaje,
+      message: subtitulo,
+      buttons: ["Ingresar"]
+    })
+
+    alrt.present()
   }
 
 }
