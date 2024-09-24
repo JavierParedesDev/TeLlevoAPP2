5204ace0cb3d4f2321e5bf5efdd34b48dceacd72
CETECOM <cetecom_conce@duoc.cl>
2024-09-24 15:47:36 -0300

    actualizacion 4

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../interfaces/usuario';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  usr:Usuario={
+    id: Date.now(),
+    nombre:"",
+    email:"",
+    contrasena:""
+  }
 
-  constructor() { }
+  constructor(
+    private alert: AlertController
+  ) { }
 
   ngOnInit() {
+  }
+
+  registro(){
+    if(!this.usr.nombre || !this.usr.email || !this.usr.contrasena){
+      this.alertas("Error!! Rellene los campos")
+    }
+  }
+
+  async alertas(mensaje: string){
+    this.alert.create({
+      header: mensaje,
+      buttons: ["Aceptar"]
+    })
   }
 
 }
